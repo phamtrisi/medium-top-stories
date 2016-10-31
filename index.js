846c7aed8b05b4870eb5db11c179e4f4be1660ed
Si Pham <spham@Sis-MacBook-Pro.local>
2016-10-31 00:21:43 -0700

    Basic json returned

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 var express = require('express');
+var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 var env = process.env;
 var port = env.PORT || 8888;
+var MEDIUM_TOP_STORIES_URL = 'https://medium.com/browse/top';
+
+function parseHtmlToJsonData(html) {
+    var $ = cheerio.load(html);
+    var $container = $('.homeContainer-stream');
+    var $topStories = $container.find('.streamItem');
+    var stories = [];
+    $topStories.each(function(idx, $story) {
+        var $story = $(this);
+        var title = $story.find('.graf--title').text();
+        var content = 'This is the content of ' + title;
+        var url = $story.find('.postArticle-content a').eq(0).attr('href');
+
+        stories.push({
+            updateDate: new Date(),
+            titleText: title,
+            mainText: content,
+            redirectionUrl: url
+        });
+    });
+
+    return stories;
+}
 
 app.get('/', function (req, res) {
-    res.json({
-        status: 'ok'
-    });
+    request(MEDIUM_TOP_STORIES_URL, function(err, resp, body) {
+        res.json(parseHtmlToJsonData(body));
+    })
 });
 
 app.listen(port, function () {
