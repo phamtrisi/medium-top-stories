ca6871b1ef21cab6ede682ce8605f2ef112908f1
Si Pham <phamtrisi@gmail.com>
2016-10-31 20:20:32 -0700

    Run update once every 10mins

--- tasks/update-top-stories/index.js
+++ tasks/update-top-stories/index.js
@@ -4,7 +4,7 @@ var medium = require('../../utils/medium');
 function updateTopStories(firebaseApp) {
     try {
         var job = new CronJob({
-            cronTime: '* * * * *', // Runs every 10 mins
+            cronTime: '*/10 * * * *', // Runs every 10 mins
             start: true,
             onTick: function() {
                 medium.updateTopStories(firebaseApp)
